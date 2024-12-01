f6f6ea66100a7ccb03d351c3db758d066b528099
Pratikkuntawar <137525410+Pratikkuntawar@users.noreply.github.com>
2024-12-01 09:15:07 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { NavLink } from 'react-router-dom';
 import logo from '../images/brain_logo.webp';
 import './Navbar.css'; // Import custom CSS for further styling
 
 function Navbar() {
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  // Toggle the navbar collapse state
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
+  // Close the navbar toggler when a link is clicked
+  const closeNavbar = () => setIsNavCollapsed(true);
+
   return (
     <>
       <nav className=" navb navbar navbar-expand-lg navbar-dark bg-danger bg-opacity-0 px-4">
         {/* Logo */}
-        <NavLink className="navbar-brand d-flex align-items-center" to="/">
+        <NavLink className="navbar-brand d-flex align-items-center" to="/" onClick={closeNavbar}>
           <img 
             src={logo} 
             alt="BrainStroke logo" 
@@ -25,28 +33,39 @@ function Navbar() {
           className="navbar-toggler" 
           type="button" 
           data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent" 
           aria-controls="navbarSupportedContent" 
-          aria-expanded="false" 
+          aria-expanded={!isNavCollapsed} 
           aria-label="Toggle navigation"
+          onClick={handleNavCollapse}
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
         {/* Navbar links */}
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div 
+          className={`collapse navbar-collapse ${isNavCollapsed ? '' : 'show'}`} 
+          id="navbarSupportedContent"
+        >
           <ul className="navbar-nav ms-auto gap-4">
             <li className="nav-item">
-              <NavLink className="nav-link text-white nav-hover" to="/" activeClassName="active">Home</NavLink> {/* Added nav-hover class */}
+              <NavLink className="nav-link text-white nav-hover" to="/" activeClassName="active" onClick={closeNavbar}>
+                Home
+              </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link text-white nav-hover" to="/about" activeClassName="active">About</NavLink>
+              <NavLink className="nav-link text-white nav-hover" to="/about" activeClassName="active" onClick={closeNavbar}>
+                About
+              </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link text-white nav-hover" to="/upload" activeClassName="active">Upload</NavLink>
+              <NavLink className="nav-link text-white nav-hover" to="/upload" activeClassName="active" onClick={closeNavbar}>
+                Upload
+              </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink className="nav-link text-white nav-hover" to="/faq" activeClassName="active">FAQ</NavLink>
+              <NavLink className="nav-link text-white nav-hover" to="/faq" activeClassName="active" onClick={closeNavbar}>
+                FAQ
+              </NavLink>
             </li>
           </ul>
         </div>
@@ -56,6 +75,65 @@ function Navbar() {
 }
 
 export default Navbar;
+
+// import React from 'react';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import { NavLink } from 'react-router-dom';
+// import logo from '../images/brain_logo.webp';
+// import './Navbar.css'; // Import custom CSS for further styling
+
+// function Navbar() {
+//   return (
+//     <>
+//       <nav className=" navb navbar navbar-expand-lg navbar-dark bg-danger bg-opacity-0 px-4">
+//         {/* Logo */}
+//         <NavLink className="navbar-brand d-flex align-items-center" to="/">
+//           <img 
+//             src={logo} 
+//             alt="BrainStroke logo" 
+//             height="60px" 
+//             width="150px" 
+//             className="me-3 logo-hover" // Added logo hover class
+//           />
+//           <h3 className="mb-0 text-white brand-name">Brain Stroke Detection</h3> {/* Brand name with custom class */}
+//         </NavLink>
+
+//         {/* Navbar toggle button for mobile view */}
+//         <button 
+//           className="navbar-toggler" 
+//           type="button" 
+//           data-bs-toggle="collapse"
+//           data-bs-target="#navbarSupportedContent" 
+//           aria-controls="navbarSupportedContent" 
+//           aria-expanded="false" 
+//           aria-label="Toggle navigation"
+//         >
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
+
+//         {/* Navbar links */}
+//         <div className="collapse navbar-collapse" id="navbarSupportedContent">
+//           <ul className="navbar-nav ms-auto gap-4">
+//             <li className="nav-item">
+//               <NavLink className="nav-link text-white nav-hover" to="/" activeClassName="active">Home</NavLink> {/* Added nav-hover class */}
+//             </li>
+//             <li className="nav-item">
+//               <NavLink className="nav-link text-white nav-hover" to="/about" activeClassName="active">About</NavLink>
+//             </li>
+//             <li className="nav-item">
+//               <NavLink className="nav-link text-white nav-hover" to="/upload" activeClassName="active">Upload</NavLink>
+//             </li>
+//             <li className="nav-item">
+//               <NavLink className="nav-link text-white nav-hover" to="/faq" activeClassName="active">FAQ</NavLink>
+//             </li>
+//           </ul>
+//         </div>
+//       </nav>
+//     </>
+//   );
+// }
+
+// export default Navbar;
 
 
 // import React from 'react';
